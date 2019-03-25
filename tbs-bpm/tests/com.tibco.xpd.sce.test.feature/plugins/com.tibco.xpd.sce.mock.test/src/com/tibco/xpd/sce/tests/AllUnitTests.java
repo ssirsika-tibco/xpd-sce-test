af5a3947a1a7db90b9775ef247787c04031733de
Phillip Watson <pwatson@tibco.com>
2019-03-25 09:01:02 +0000

    ACE-405: New SCE mock test plugin
    svn path=/trunk/; revision=89366

---
+++ tbs-bpm/tests/com.tibco.xpd.sce.test.feature/plugins/com.tibco.xpd.sce.mock.test/src/com/tibco/xpd/sce/tests/AllUnitTests.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) TIBCO Software Inc 2004, 2019. All rights reserved.
+ */
+
+package com.tibco.xpd.sce.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * All SCE unit tests.
+ *
+ * @author pwatson
+ * @since 21 Mar 2019
+ */
+@SuppressWarnings("nls")
+public class AllUnitTests {
+    public static Test suite() {
+        TestSuite suite =
+                new TestSuite("Test for com.tibco.xpd.sce.mock.tests");
+
+        // suite.addTestSuite(PERascContributorTest.class);
+
+        return suite;
+    }
+}
