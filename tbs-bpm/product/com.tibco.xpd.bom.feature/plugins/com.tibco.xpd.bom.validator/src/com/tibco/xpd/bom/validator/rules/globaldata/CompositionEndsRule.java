c98665fed6fc221b9874cec5596c61a4c31e3982
Sid Allway <aallway@tibco.com>
2019-05-23 13:10:11 +0000

    ACE-1358: Remove unnecessary Problem Markers related to invalid inter-bom BOM data references
    svn path=/trunk/; revision=89748

--- tbs-bpm/product/com.tibco.xpd.bom.feature/plugins/com.tibco.xpd.bom.validator/src/com/tibco/xpd/bom/validator/rules/globaldata/CompositionEndsRule.java
+++ tbs-bpm/product/com.tibco.xpd.bom.feature/plugins/com.tibco.xpd.bom.validator/src/com/tibco/xpd/bom/validator/rules/globaldata/CompositionEndsRule.java
@@ -4,25 +4,14 @@
 
 package com.tibco.xpd.bom.validator.rules.globaldata;
 
-import static com.tibco.xpd.bom.globaldata.resources.GlobalDataProfileManager.StereotypeKind.CASE;
-import static com.tibco.xpd.bom.globaldata.resources.GlobalDataProfileManager.StereotypeKind.GLOBAL;
-
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.uml2.uml.AggregationKind;
 import org.eclipse.uml2.uml.Association;
-import org.eclipse.uml2.uml.Class;
-import org.eclipse.uml2.uml.Classifier;
 import org.eclipse.uml2.uml.Property;
 
 import com.tibco.xpd.bom.globaldata.resources.GlobalDataProfileManager.StereotypeKind;
-import com.tibco.xpd.bom.resources.utils.BOMUtils;
-import com.tibco.xpd.bom.resources.utils.UML2ModelUtil;
 import com.tibco.xpd.bom.validator.internal.Messages;
 import com.tibco.xpd.bom.validator.util.BOMValidationUtil;
 import com.tibco.xpd.validation.provider.IValidationScope;
@@ -36,12 +25,6 @@ public class CompositionEndsRule extends BaseAssociationEndsRule {
 
     protected static final String ISSUE_ID_UNSUPPORTED_BIDIRECTIONAL =
             "bom.globaldata.composition.bidirectional.unsupported.issue"; //$NON-NLS-1$
-
-    protected static final String INTRABOM_ISSUE_ID =
-            "bom.globaldata.composition.unidirectional.intrabom.issue"; //$NON-NLS-1$   
-
-    protected static final String INTRABOM_HAS_MUTABLE_SOLUTION_ISSUE_ID =
-            "bom.globaldata.composition.unidirectional.intrabom.mutable.issue"; //$NON-NLS-1$
 
     protected static final String ISSUE_ID_UNSUPPORTED_MULTIPLICITY =
             "bom.globaldata.composition.multiplicity.issue"; //$NON-NLS-1$
@@ -84,17 +67,6 @@ public class CompositionEndsRule extends BaseAssociationEndsRule {
                         partclassTypes);
 
         if (compositionHasValidClasses) {
-            if (BOMUtils.isIntraBomRelationship(association)) {
-                doIntraBomUnidirectionalValidation(association,
-                        scope,
-                        wholeclassTypes,
-                        partclassTypes);
-            } else {
-                doInterBomUnidirectionalValidation(scope,
-                        association,
-                        wholeclassTypes,
-                        partclassTypes);
-            }
 
             // Add a check to ensure that the start of the composition
             // is not multiple
@@ -105,159 +77,6 @@ public class CompositionEndsRule extends BaseAssociationEndsRule {
                             association.eResource().getURIFragment(association));
                 }
             }
-        }
-    }
-
-    /**
-     * @param scope
-     * @param association
-     * @param wholeclassTypes
-     * @param partclassTypes
-     */
-    private void doInterBomUnidirectionalValidation(IValidationScope scope,
-            Association association, Set<StereotypeKind> wholeclassTypes,
-            Set<StereotypeKind> partclassTypes) {
-
-        String[] msgParams = null;
-
-        if (wholeclassTypes.contains(CASE)) {
-            if (partclassTypes.contains(CASE)) {
-                // disallowed!
-                msgParams =
-                        new String[] { WHOLECLASS_ARG, CASETYPE_ARG,
-                                PARTCLASS_ARG, CASETYPE_ARG };
-            } else if (partclassTypes.contains(GLOBAL)) {
-                // disallowed!
-                msgParams =
-                        new String[] { WHOLECLASS_ARG, CASETYPE_ARG,
-                                PARTCLASS_ARG, GLOBALTYPE_ARG };
-            } else { // whole class is LOCAL
-                // disallowed: whole should have mutated to 'global'
-                msgParams =
-                        new String[] { WHOLECLASS_ARG, CASETYPE_ARG,
-                                PARTCLASS_ARG, LOCALTYPE_ARG };
-            }
-        } else if (wholeclassTypes.contains(GLOBAL)) {
-            if (partclassTypes.contains(CASE)) {
-                // disallowed!
-                msgParams =
-                        new String[] { WHOLECLASS_ARG, GLOBALTYPE_ARG,
-                                PARTCLASS_ARG, CASETYPE_ARG };
-            } else if (partclassTypes.contains(GLOBAL)) {
-                // disallowed!
-                msgParams =
-                        new String[] { WHOLECLASS_ARG, GLOBALTYPE_ARG,
-                                PARTCLASS_ARG, GLOBALTYPE_ARG };
-            } else { // part class is LOCAL
-                // disallowed: whole should have mutated to 'global'
-                msgParams =
-                        new String[] { WHOLECLASS_ARG, GLOBALTYPE_ARG,
-                                PARTCLASS_ARG, LOCALTYPE_ARG };
-            }
-        } else { // whole class is LOCAL
-            if (partclassTypes.contains(CASE)) {
-                // allowed
-            } else if (partclassTypes.contains(GLOBAL)) {
-                // allowed
-            } else { // part class is LOCAL
-                // allowed
-            }
-        }
-
-        if (msgParams != null && msgParams.length == 4) {
-
-            Map<String, String> additionalInfo = new HashMap<String, String>();
-            additionalInfo.put(RELATIONSHIP_LOCATION, association.eResource()
-                    .getURIFragment(association));
-
-            // get whole end to set issue against
-            List<Class> associationEnds =
-                    UML2ModelUtil.getAssociationEnds(association);
-            if (associationEnds.size() >= 2) {
-                Classifier classifier = associationEnds.get(0);
-                if (classifier != null) {
-
-                    scope.createIssue(INTRABOM_ISSUE_ID,
-                            BOMValidationUtil.getLocation(classifier),
-                            classifier.eResource().getURIFragment(classifier),
-                            Arrays.asList(msgParams),
-                            additionalInfo);
-                }
-            }
-        }
-    }
-
-    /**
-     * @param association
-     * @param scope
-     * @param wholeclassTypes
-     * @param partclassTypes
-     */
-    private void doIntraBomUnidirectionalValidation(Association association,
-            IValidationScope scope, Set<StereotypeKind> wholeclassTypes,
-            Set<StereotypeKind> partclassTypes) {
-
-        // Classifier wholeClass = getWholeClass(association);
-        Classifier partClass = getPartClass(association);
-
-        ClassMutationSolution mutationSolution = null;
-        String[] msgParams = null;
-
-        if (wholeclassTypes.contains(CASE)) {
-            if (partclassTypes.contains(CASE)) {
-                // disallowed!
-                msgParams =
-                        new String[] { WHOLECLASS_ARG, CASETYPE_ARG,
-                                PARTCLASS_ARG, CASETYPE_ARG };
-            } else if (partclassTypes.contains(GLOBAL)) {
-                // allowed
-            } else { // whole class is LOCAL
-                // disallowed!
-                msgParams =
-                        new String[] { WHOLECLASS_ARG, CASETYPE_ARG,
-                                PARTCLASS_ARG, LOCALTYPE_ARG };
-                mutationSolution =
-                        new ClassMutationSolution(GLOBAL, partClass,
-                                INTRABOM_HAS_MUTABLE_SOLUTION_ISSUE_ID);
-            }
-        } else if (wholeclassTypes.contains(GLOBAL)) {
-            if (partclassTypes.contains(CASE)) {
-                // disallowed!
-                msgParams =
-                        new String[] { WHOLECLASS_ARG, GLOBALTYPE_ARG,
-                                PARTCLASS_ARG, CASETYPE_ARG };
-            } else if (partclassTypes.contains(GLOBAL)) {
-                // allowed
-            } else { // part class is LOCAL
-                // disallowed!
-                msgParams =
-                        new String[] { WHOLECLASS_ARG, GLOBALTYPE_ARG,
-                                PARTCLASS_ARG, LOCALTYPE_ARG };
-                mutationSolution =
-                        new ClassMutationSolution(GLOBAL, partClass,
-                                INTRABOM_HAS_MUTABLE_SOLUTION_ISSUE_ID);
-            }
-        } else { // whole class is LOCAL
-            if (partclassTypes.contains(CASE)) {
-                // allowed
-            } else if (partclassTypes.contains(GLOBAL)) {
-                // allowed
-            } else { // part class is LOCAL
-                // allowed
-            }
-        }
-
-        if (msgParams != null && msgParams.length == 4) {
-
-            String issueID =
-                    (mutationSolution == null) ? INTRABOM_ISSUE_ID
-                            : mutationSolution.issueID;
-
-            scope.createIssue(issueID,
-                    BOMValidationUtil.getLocation(association),
-                    association.eResource().getURIFragment(association),
-                    Arrays.asList(msgParams),
-                    getAdditionalInfo(mutationSolution, association));
         }
     }
 
---
+++ tbs-bpm/tests/com.tibco.xpd.sce.test.feature/plugins/com.tibco.xpd.sce.test/src/com/tibco/xpd/sce/tests/validation/AceAllowCrossClassTypeAndCrosssProjectReferenceTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) TIBCO Software Inc 2004, 2019. All rights reserved.
+ */
+
+package com.tibco.xpd.sce.tests.validation;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+
+import com.tibco.xpd.core.test.util.TestUtil;
+import com.tibco.xpd.resources.util.ProjectImporter;
+
+import junit.framework.TestCase;
+
+/**
+ * In AMX BPM there were various rules preventing the use of one class type
+ * (Case/Local) from other types. There were also rules governing the use of
+ * types across projects for case classes. These rules have been removed for ACE
+ * as they are no longer required restrictions for the runtime or for BOM to
+ * CaseDataModel transformation
+ * 
+ * The test projects used here break these rules, so this test ensures that they
+ * projects are problem free.
+ * 
+ * @author aallway
+ * @since 22 Mar 2019
+ */
+public class AceAllowCrossClassTypeAndCrosssProjectReferenceTest extends TestCase {
+
+    // @Test
+    public void testLocalBOMDataProjectMigration() {
+        ProjectImporter projectImporter = importMainTestProjects();
+
+        assertFalse(
+                "TestCrossClassTypeRulesRemoved_1" //$NON-NLS-1$
+                        + " project should not have any problem markers", //$NON-NLS-1$
+                hasProblemMarker(
+                        ResourcesPlugin.getWorkspace().getRoot()
+                                .getProject(
+                                        "TestCrossClassTypeRulesRemoved_1"))); //$NON-NLS-1$
+
+        assertFalse("TestCrossClassTypeRulesRemoved_2" //$NON-NLS-1$
+                + " project should not have any problem markers", //$NON-NLS-1$
+                hasProblemMarker(ResourcesPlugin.getWorkspace().getRoot()
+                        .getProject("TestCrossClassTypeRulesRemoved_2"))); //$NON-NLS-1$
+
+        projectImporter.performDelete();
+    }
+
+    /**
+     * Import all projects from test plugin resources for the main test
+     * 
+     * @return the project importer
+     */
+    private ProjectImporter importMainTestProjects() {
+        /*
+         * Import and mgirate the project
+         */
+
+        ProjectImporter projectImporter = TestUtil.importProjectsFromZip(
+                "com.tibco.xpd.sce.test", //$NON-NLS-1$
+                new String[] {
+                        "resources/CrossClassTypeAndCrosssProjectTest/TestCrossClassTypeRulesRemoved_1/", //$NON-NLS-1$
+                        "resources/CrossClassTypeAndCrosssProjectTest/TestCrossClassTypeRulesRemoved_2/" }, //$NON-NLS-1$
+                new String[] { "TestCrossClassTypeRulesRemoved_1", //$NON-NLS-1$
+                        "TestCrossClassTypeRulesRemoved_2" }); //$NON-NLS-1$
+
+        assertTrue(
+                "Failed to load projects from resources/CrossClassTypeAndCrosssProjectTest/" //$NON-NLS-1$
+                        + "TestCrossClassTypeRulesRemoved_1 or TestCrossClassTypeRulesRemoved_2", //$NON-NLS-1$
+                projectImporter != null);
+
+        TestUtil.buildAndWait();
+
+        return projectImporter;
+    }
+
+    /**
+     * 
+     * @param resource
+     * @param markerId
+     * @return <code>true</code> if given resource has given problem marker
+     *         raised on it.
+     */
+    private boolean hasProblemMarker(IResource resource) {
+        try {
+            IMarker[] markers = resource
+                    .findMarkers(IMarker.PROBLEM,
+                            true,
+                            IResource.DEPTH_INFINITE);
+
+            if (markers != null && markers.length > 0) {
+                return true;
+            }
+
+        } catch (CoreException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+}
