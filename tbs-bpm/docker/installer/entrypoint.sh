1554d0e76d7bcd3e4fa58033f439f59b92c51095
Phillip Watson <pwatson@tibco.com>
2019-06-10 12:25:24 +0000

    ACE-1138: Change exposed folders to plurals.
    svn path=/trunk/; revision=89861

--- tbs-bpm/docker/installer/entrypoint.sh
+++ tbs-bpm/docker/installer/entrypoint.sh
@@ -3,7 +3,7 @@
 # if the argument "export" is passed on the command-line
 if [[ -n "$1" ]] && [[ "$1" = "export" ]]; then
   # run the export ant task
-  exec ./ant-runner -f ./build.xml -data $WORKSPACE_DIR -Dworkspace.dir=$WORKSPACE_DIR -Dproject.dir=$PROJECT_DIR -Dexport.dir=$EXPORT_DIR
+  exec ./ant-runner -f ./build.xml -data $WORKSPACE_DIR -Dworkspace.dir=$WORKSPACE_DIR -Dprojects.dir=$PROJECTS_DIR -Dexports.dir=$EXPORTS_DIR
 
 else
   # start a studio session
--- tbs-bpm/docker/installer/run-docker-example.bat
+++ tbs-bpm/docker/installer/run-docker-example.bat
@@ -3,5 +3,13 @@ docker build -t tibco/sce-studio:5.0.0 .
 docker run --rm -v //c/eclipse/oxygen/workspaces/runtime-SCE:/project -v //c/scratch/temp/eclipse/export:/export tibco/sce-studio:5.0.0 export
 
 set DISPLAY=10.100.83.72:0.0
-docker run -it --rm --name eclipse -e DISPLAY=%DISPLAY% -v//c/eclipse/oxygen/workspaces/SCE-docker:/workspace -v //c/eclipse/oxygen/workspaces/runtime-SCE:/project tibco/sce-studio:5.0.0
+docker run -it --rm --name studio -e DISPLAY -v//c/eclipse/oxygen/workspaces/SCE-docker:/workspace -v //c/eclipse/oxygen/workspaces/runtime-SCE:/project tibco/sce-studio:5.0.0
 
+docker run -it --rm --name studio -e DISPLAY -v //c/eclipse/oxygen/workspaces/runtime-SCE:/workspace -v //c/scratch/temp/eclipse/export:/export tibco/sce-studio:5.0.0
+
+docker run -it --rm --name studio --entrypoint bash tibco/sce-studio:5.0.0
+
+
+
+
+docker run --rm -v //c/scratch/temp/first_mactel:/project -v //c/scratch/temp/eclipse/export:/export tibco/sce-studio:5.0.0 export
