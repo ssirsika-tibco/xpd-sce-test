79dedb1c9d210c3170eb626fa787348e01e0f357
Phillip Watson <pwatson@tibco.com>
2019-06-13 12:26:38 +0000

    ACE-1137: add some examples of how to use docker image
    svn path=/trunk/; revision=89895

--- tbs-bpm/docker/run-docker-example.bat
+++ tbs-bpm/docker/run-docker-example.bat
@@ -1,15 +1,12 @@
-docker build -t tibco/sce-studio:5.0.0 .
+docker build -t tibco/sce-studio:5.0.0 .\image_template
 
-docker run --rm -v //c/eclipse/oxygen/workspaces/runtime-SCE:/project -v //c/scratch/temp/eclipse/export:/export tibco/sce-studio:5.0.0 export
+# run ANT export on projects within c:\eclipse\oxygen\workspaces\runtime-SCE - output to c:\scratch\temp\eclipse\export
+docker run --rm -v //c/eclipse/oxygen/workspaces/runtime-SCE:/projects -v //c/scratch/temp/eclipse/export:/exports tibco/sce-studio:5.0.0 export
 
+# start SCE studio within docker image - open workspace c:\eclipse\oxygen\workspaces\runtime-SCE and exports folder mapped
 set DISPLAY=10.100.83.72:0.0
-docker run -it --rm --name studio -e DISPLAY -v//c/eclipse/oxygen/workspaces/SCE-docker:/workspace -v //c/eclipse/oxygen/workspaces/runtime-SCE:/project tibco/sce-studio:5.0.0
+docker run -it --rm --name studio -e DISPLAY -v//c/eclipse/oxygen/workspaces/runtime-SCE:/workspace -v //c/scratch/temp/eclipse/export:/exports tibco/sce-studio:5.0.0
 
-docker run -it --rm --name studio -e DISPLAY -v //c/eclipse/oxygen/workspaces/runtime-SCE:/workspace -v //c/scratch/temp/eclipse/export:/export tibco/sce-studio:5.0.0
+# start SCE docker container in bash shell - with workspace and exports folders mapped
+docker run -it --rm --name studio -v//c/eclipse/oxygen/workspaces/runtime-SCE:/workspace //c/scratch/temp/eclipse/export:/exports --entrypoint bash tibco/sce-studio:5.0.0
 
-docker run -it --rm --name studio --entrypoint bash tibco/sce-studio:5.0.0
-
-
-
-
-docker run --rm -v //c/scratch/temp/first_mactel:/project -v //c/scratch/temp/eclipse/export:/export tibco/sce-studio:5.0.0 export
