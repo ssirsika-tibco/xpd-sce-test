c40b4422a7bc5a09702e6750629f224ab52a63b1
Phillip Watson <pwatson@tibco.com>
2019-07-16 13:13:07 +0000

    ACE-1147: check workspace content and delete if it contains metadata
    svn path=/trunk/; revision=90292

--- tbs-bpm/docker/image_template/entrypoint.sh
+++ tbs-bpm/docker/image_template/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 # if the argument "export" is passed on the command-line
-if [[ -n "$1" ]] && [[ "$1" = "export" ]]; then
+if [[ -n "$1" ]] && [[ "$1" == "export" ]]; then
+  # if workspace contains metadata folder
+  if [[ -d "${WORKSPACE_DIR}/.metadata" ]]; then
+    # remove workspace dir content
+    find $WORKSPACE_DIR -mindepth 1 -delete
+  fi
+  
   # run the export ant task
   exec ./ant-runner -f ./build.xml -data $WORKSPACE_DIR -Dworkspace.dir=$WORKSPACE_DIR -Dprojects.dir=$PROJECTS_DIR -Dexports.dir=$EXPORTS_DIR
 
