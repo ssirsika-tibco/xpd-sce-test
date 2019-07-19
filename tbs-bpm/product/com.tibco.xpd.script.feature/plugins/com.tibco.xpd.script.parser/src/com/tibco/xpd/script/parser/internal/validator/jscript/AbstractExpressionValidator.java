dfbb6da69fdcb0ecec6f38c04b9993c598733a2e
Nick Wilson <nwilson@tibco.com>
2019-07-19 11:25:26 +0000

    ACE-1837: Fixed direct array assignment message.
    svn path=/trunk/; revision=90314

--- tbs-bpm/product/com.tibco.xpd.script.feature/plugins/com.tibco.xpd.script.parser/src/com/tibco/xpd/script/parser/internal/validator/jscript/AbstractExpressionValidator.java
+++ tbs-bpm/product/com.tibco.xpd.script.feature/plugins/com.tibco.xpd.script.parser/src/com/tibco/xpd/script/parser/internal/validator/jscript/AbstractExpressionValidator.java
@@ -2224,6 +2224,9 @@ public abstract class AbstractExpressionValidator extends AbstractValidator
                     isReadOnly,
                     extendedInfo);
         }
+        if (matchingProperty != null && matchingProperty.isArray()) {
+            isReadOnly = true;
+        }
         if (matchingProperty instanceof ITypeResolution) {
             if (isContextReadOnly) {
                 isReadOnly = isContextReadOnly;
