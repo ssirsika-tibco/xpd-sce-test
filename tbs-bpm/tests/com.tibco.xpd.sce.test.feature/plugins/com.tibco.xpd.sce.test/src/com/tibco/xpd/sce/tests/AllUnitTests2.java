42e49e91b47a5bfd33c9b64d3aaed6267184274b
Phillip Watson <pwatson@tibco.com>
2019-08-01 09:09:48 +0000

    ACE-2185: Refactory date creator and methods to use standard JS Date class.
    svn path=/trunk/; revision=90409

--- tbs-bpm/tests/com.tibco.xpd.sce.test.feature/plugins/com.tibco.xpd.sce.test/src/com/tibco/xpd/sce/tests/AllUnitTests2.java
+++ tbs-bpm/tests/com.tibco.xpd.sce.test.feature/plugins/com.tibco.xpd.sce.test/src/com/tibco/xpd/sce/tests/AllUnitTests2.java
@@ -12,6 +12,7 @@ import com.tibco.xpd.sce.tests.brm.transform.BRMGenUC2FTest;
 import com.tibco.xpd.sce.tests.ce.destination.BpmProjectMustBeCeTest;
 import com.tibco.xpd.sce.tests.ce.destination.NewCeProjectsTest;
 import com.tibco.xpd.sce.tests.importmigration.Bpm2CeProjectMigrationTest;
+import com.tibco.xpd.sce.tests.importmigration.ScriptArrayMigrationTests;
 import com.tibco.xpd.sce.tests.importmigration.ScriptMigrationTests;
 import com.tibco.xpd.sce.tests.javascript.AceDataWrapperScriptObjectTest;
 import com.tibco.xpd.sce.tests.javascript.AceGlobalSignalMappingGenerationTest;
@@ -42,6 +43,7 @@ public class AllUnitTests2 {
          */
         suite.addTestSuite(Bpm2CeProjectMigrationTest.class);
         suite.addTestSuite(ScriptMigrationTests.class);
+        suite.addTestSuite(ScriptArrayMigrationTests.class);
 
         suite.addTestSuite(AceGlobalSignalDataMapperTest.class);
         suite.addTestSuite(NewCeProjectsTest.class);
